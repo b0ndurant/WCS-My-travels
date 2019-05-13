1d7081177ca17c3ea0ef9653e8da81b0292cf9ce
b0ndurant <martin.aurelien01@gmail.com>
2019-05-13 16:08:39 +0200

    display a list of components : React Exercise 2

--- src/App.js
+++ src/App.js
@@ -6,22 +6,7 @@ function App() {
   return (
     <div className="container">
       <div className="row">
-        <div className="col-md-6">
-          <Travel
-            destinations="Rio"
-            country="Brésil"
-            picture="https://lepetitjournal.com/sites/default/files/styles/main_article/public/2018-03/Br%C3%A9sil.jpg?itok=KPPBy_Qc"
-            distance="8620 km"
-          />
-        </div>
-        <div className="col-md-6">
-          <Travel
-            destinations="Dubai"
-            country="Emirats arabes unis"
-            picture="https://images.salaun-holidays.com/(Image)-image-emirats-arabes-unis-abu-dhabi-01-is_517465184.jpg"
-            distance="6702 km"
-          />
-        </div>
+        <Travel />
       </div>
     </div>
   );
--- src/components/Travel.js
+++ src/components/Travel.js
@@ -1,13 +1,47 @@
 import React from 'react';
 
+const trips = [
+    {
+        destinations: "Rio",
+        country: "Brésil",
+        picture: "https://lepetitjournal.com/sites/default/files/styles/main_article/public/2018-03/Br%C3%A9sil.jpg?itok=KPPBy_Qc",
+        distance: 8620,
+    },
+    {
+        destinations: "Dubai",
+        country: "Emirats arabes unis",
+        picture: "https://images.salaun-holidays.com/(Image)-image-emirats-arabes-unis-abu-dhabi-01-is_517465184.jpg",
+        distance: "6702 km",
+    },
+    {
+        destinations: "Paris",
+        country: "France",
+        picture: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Paris_Night.jpg/713px-Paris_Night.jpg",
+        distance: 200,
+    },
+    {
+        destinations: "Londre",
+        country: "Angleterre",
+        picture: "https://www.lonelyplanet.fr/sites/lonelyplanet/files/styles/manual_crop/public/media/destination/slider/mobile/500px_55425108_2.jpg?itok=FM16--08",
+        distance: 500,
+    },
+    {
+        destinations: "Rome",
+        country: "Italie",
+        picture: "https://www.nacel.fr/medias/_cache/pays/17/imagePrincipale/1920_1440/sejour-linguistique-italie.jpg",
+        distance: 1575,
+    },
+]
 const Travel = ({ destinations, country, picture, distance }) => {
     return (
-        <>
-            <div>Destinations : {destinations}</div>
-            <div>Pays : {country}</div>
-            <div>Image : <img src={picture} alt="Brazil" /></div>
-            <div>Distance : {distance}</div>
-        </>
+        trips.map(trip => (
+            <div className="col-md-4">
+                <div>Destinations : {trip.destinations}</div>
+                <div>Pays : {trip.country}</div>
+                <div>Image : <img src={trip.picture} alt="Brazil" /></div>
+                <div>Distance : {trip.distance} km</div>
+            </div>
+        ))
     )
 }
 
