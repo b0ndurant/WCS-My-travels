06d51534b92c047ba70a7a0056f95409d59c4e8e
b0ndurant <martin.aurelien01@gmail.com>
2019-05-13 16:15:40 +0200

    Updates

---
+++ precache-manifest.63be0148320249918546c7f64ee8dc47.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79452ec562ba4bcf73824b7e642d7775",
+    "url": "/b0ndurant/WCS-My-travels/index.html"
+  },
+  {
+    "revision": "4b1e4cc9dcd8c952adfb",
+    "url": "/b0ndurant/WCS-My-travels/static/css/main.a25a1599.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/b0ndurant/WCS-My-travels/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "4b1e4cc9dcd8c952adfb",
+    "url": "/b0ndurant/WCS-My-travels/static/js/main.a25a0cc5.chunk.js"
+  },
+  {
+    "revision": "1733cf80f17f7326eba1",
+    "url": "/b0ndurant/WCS-My-travels/static/js/runtime~main.e266517d.js"
+  }
+]);
